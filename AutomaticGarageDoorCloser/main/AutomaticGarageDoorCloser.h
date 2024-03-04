5ae0066348a95bfeb8bc4886873e839dd5baaafd
Ashton Fritz <55860949+acf879@users.noreply.github.com>
2024-03-04 03:29:32 -0600

    Added mac address scanner but disabled tasks
    Currently tasks are disabled due to watchdog timer being triggered. This will be undone as the tasks actually perform something to reset the watchdog timer. Currently limited to the top 20 strongest signals but does not currently do bluetooth so second controller may be required to be plugged in to act as a transmitter. Webserver needs to be added along with reading in signal to check if the door is open.

--- AutomaticGarageDoorCloser/main/AutomaticGarageDoorCloser.h
+++ AutomaticGarageDoorCloser/main/AutomaticGarageDoorCloser.h
@@ -1,9 +1,17 @@
 #include "freertos/task.h"
+#include <string.h>
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
 
 #ifndef AutomaticGarageDoorCloser_h
 #define AutomaticGarageDoorCloser_h
 
 #define MAX_STACK_SIZE 4096
+
+#define DEFAULT_SCAN_LIST_SIZE 20
 
 #define DOOR_OPEN 1
 #define DOOR_CLOSED 0
@@ -27,5 +35,8 @@ void createBeaconTask(void);
 void WebserverTask(void *pvParameters);
 void DoorSensorTask(void *pvParameters);
 void BeaconTask(void *pvParameters);
+
+void wifi_init(void);
+static void wifi_scan(void);
 
 #endif
--- AutomaticGarageDoorCloser/main/main.c
+++ AutomaticGarageDoorCloser/main/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
-//#include "freertos/task.h"
 #include "AutomaticGarageDoorCloser.h"
+#include "esp_wifi.h"
+#include "esp_task_wdt.h"
 
 void app_main(void)
 {
@@ -9,16 +10,23 @@ void app_main(void)
     for (;;)
     {
         // Do something
-        printf("Main\n");
+        wifi_scan();
     }
 }
 
 void initialization(void)
 {
     printf("Initializing...\n");
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+    wifi_init();
     gpioSetup();
-    taskSetup();
-    vTaskDelete(NULL); // Delete the initial task
+    //taskSetup();
+    //vTaskDelete(NULL); // Delete the initial task
 }
 
 void gpioSetup(void)
@@ -31,8 +39,8 @@ void taskSetup(void)
 {
     printf("Setting up tasks...\n");
     createBeaconTask();
-    createDoorSensorTask();
-    createWebserverTask();
+    //createDoorSensorTask();
+    //createWebserverTask();
     printf("Tasks all set up.\n");
 }
 
@@ -67,14 +75,13 @@ void createDoorSensorTask(void)
 {
     printf("Creating door sensor task...\n");
     // Create door sensor task
-    xTaskCreatePinnedToCore(
+    xTaskCreate(
         DoorSensorTask, 
-        "Door Sensor Task", 
+        "DoorSensor Task", 
         MAX_STACK_SIZE, 
         NULL, 
-        2, 
-        &xHandleDoorSensorTask, 
-        1
+        1, 
+        &xHandleDoorSensorTask
     );
 }
 
@@ -87,8 +94,8 @@ void DoorSensorTask(void *pvParameters)
         doorTimer++;
         // Do something
         if (doorTimer == DOOR_SENSOR_INTERVAL) {
-            printf("Door status\n");
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            
+            doorTimer = 0;
         }
     }
 }
@@ -97,28 +104,59 @@ void createBeaconTask(void)
 {
     printf("Creating beacon task...\n");
     // Create beacon task
-    xTaskCreatePinnedToCore(
+    xTaskCreate(
         BeaconTask, 
         "Beacon Task", 
         MAX_STACK_SIZE, 
         NULL, 
-        1, 
-        &xHandleBeaconTask, 
-        0
+        2, 
+        &xHandleBeaconTask
     );
 }
 
 void BeaconTask(void *pvParameters)
 {
+
     printf("Beacon task running...\n");
     uint32_t beaconTimer = 0;
-    for (;;)
-    {
+
+    for (;;) {
         beaconTimer++;
         // Do something
         if (beaconTimer == BEACON_INTERVAL) {
             printf("Beacon\n");
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            wifi_scan();
         }
+    }
+}
+
+void wifi_init(void) {
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    assert(sta_netif);
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+void wifi_scan(void) {
+    uint16_t ap_count = 0;
+    uint16_t number = DEFAULT_SCAN_LIST_SIZE;
+    wifi_ap_record_t ap_info[DEFAULT_SCAN_LIST_SIZE];
+
+    esp_wifi_scan_start(NULL, true);
+    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, ap_info));
+    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));
+    printf("Total APs scanned = %u\n", ap_count);
+    for (int i = 0; (i < DEFAULT_SCAN_LIST_SIZE) && (i < ap_count); i++) {
+        printf("SSID \t\t%s", ap_info[i].ssid);
+        printf("RSSI \t\t%d", ap_info[i].rssi);
+        printf("BSSID \t\t%02X%02X%02X%02X%02X%02X\n", 
+            ap_info[i].bssid[0], ap_info[i].bssid[1], ap_info[i].bssid[2], 
+            ap_info[i].bssid[3], ap_info[i].bssid[4], ap_info[i].bssid[5]);
     }
 }
